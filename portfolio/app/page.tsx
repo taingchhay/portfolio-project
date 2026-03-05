6a960cc9e83fac09a9840c6d1477c696d4631493
taingchhay <filmakame@gmail.com>
2026-03-05 19:26:18 +0700

    feat: compose homepage layout and add public assets
    - Create main page composing all sections (Navbar, Hero, About, Skills, Projects, Contact, Footer)
    - Add public assets: favicon, SVG icons, CV PDF, and about photo
    - Add project README with setup instructions

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import SkillsSection from "./components/SkillsSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
